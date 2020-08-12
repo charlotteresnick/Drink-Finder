b42e102605dfcdd53bcd3e1dddcb3547beab9bcc
Charlotte Resnick <charlotte.resnick@gmail.com>
2020-08-11 22:37:14 -0400

    html and css for all pages in progress

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
---
+++ views/index.js
@@ -0,0 +1,5 @@
+<%- include('./partials/boilerplate') %>
+
+<h1>Check out my todos</h1>
+
+<%- include('./partials/end') %>
